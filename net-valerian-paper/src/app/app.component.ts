9d4dac85e3123324777b0f51df47c80d25ea7298
papervalerian <146490793+papervalerian@users.noreply.github.com>
2025-07-28 22:02:34 -0700

    Updates 4

--- net-valerian-paper/src/app/app.component.ts
+++ net-valerian-paper/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,HomepageComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, HomepageComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'net-valerian-paper';

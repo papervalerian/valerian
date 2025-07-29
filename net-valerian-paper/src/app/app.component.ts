662168b5c46736ff63374581971ace0d79091357
papervalerian <146490793+papervalerian@users.noreply.github.com>
2025-07-28 18:25:39 -0700

    Created App and Firest Component (Homepage)

---
+++ net-valerian-paper/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { HomepageComponent } from './homepage/homepage.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,HomepageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'net-valerian-paper';
+}
